132ce8e927a83d89f013d3f716114df99b3f385c
Josh Abrams <jabrams.nc@gmail.com>
2022-03-19 18:12:05 -0400

    added running text element

---
+++ js/effects.js
@@ -0,0 +1,21 @@
+function run() {
+    $area = $("#title-text-container")
+    var msgs = [
+        "Stop that!",
+        "Why are you following me?!",
+        "Go away!",
+        "Don't touch me!",
+        "Don't you have anything better to do?!"
+    ];
+
+    var randX = Math.floor(Math.random() * ($area.width() - 100));
+    var randY = Math.floor(Math.random() * ($area.height() - 100));
+    var index = Math.floor(Math.random() * msgs.length);
+    var msg = msgs[index];
+    $(this).html(msg);
+    $(this).stop().animate({"left": randX + "px", "top": randY + "px"});
+}
+
+$(document).ready(function() {
+    $(".runner").on("mouseenter", run);
+});
